e74d087d9a978ad4e20bee749d9a21801eaaec24
Victor Garcia <victorgf2011@gmail.com>
2023-10-26 17:55:09 +0200

    feat: favicon

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -28,7 +28,7 @@ async function render(pageContext) {
 		<html lang="en">
 		<head>
 			<meta charset="UTF-8" />
-			<link rel="icon" type="image/svg+xml" href="/vite.svg" />
+			<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta name="color-scheme" content="light dark" />
 			<meta name="description" content="${desc}" />
