8989995ddf70facade9f7dfdb10b0f22320a165b
Victor Garcia <victorgf2011@gmail.com>
2024-01-03 19:30:01 +0100

    feat: padding projects mobile first

--- pages/projects.page.jsx
+++ pages/projects.page.jsx
@@ -148,7 +148,7 @@ export function Page() {
   })
 
   return (
-    <main className="p-6 flex flex-col gap-8 max-w-xl text-justify mix-blend-exclusion">
+    <main className="p-4 sm:p-6 flex flex-col gap-8 max-w-xl text-justify mix-blend-exclusion">
       <h1 className="text-6xl">projects</h1>
 
       <div className="p-4 border-primary">
