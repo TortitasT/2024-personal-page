0918fc77258ed602d30bcfcd6b3d3741397b4685
Tortitas <victorgf2011@gmail.com>
2024-01-04 10:45:31 +0100

    feat: about data

--- pages/about.page.jsx
+++ pages/about.page.jsx
@@ -1,12 +1,33 @@
 export function Page() {
+  function dateDiffDays(date1, date2) {
+    const diff = Math.abs(date1.getTime() - date2.getTime())
+    return Math.ceil(diff / (1000 * 3600 * 24))
+  }
+
+  function daysToYears(days) {
+    return Math.round(days / 365)
+  }
+
+  const age = new Date().getFullYear() - 2004
+  const descomExperience = daysToYears(
+    dateDiffDays(new Date(2022, 2, 1), new Date())
+  )
+
+  const experience = {
+    nuxt: descomExperience,
+    laravel: descomExperience,
+    aws: descomExperience,
+  }
+
   return (
     <main className="p-6 flex flex-col gap-3 max-w-xl text-justify mix-blend-exclusion">
       <h1 className="text-6xl">about</h1>
 
       <p>
-        Hi, I'm Victor Garcia, a web developer, indie game developer and
-        programming enthusiast.
+        Hi, I'm Victor Garcia, a {age} years old web developer, indie game
+        developer and programming enthusiast.
       </p>
+
       <p>
         I work as a developer at{' '}
         <a href="https://descom.es" target="_blank">
@@ -17,6 +38,14 @@ export function Page() {
         <span class="text--nuxt">Nuxt.js</span> and{' '}
         <span class="text--laravel">Laravel</span>.
       </p>
+
+      <p>
+        I currently have {experience.nuxt} years of experience with{' '}
+        <span class="text--nuxt">Nuxt.js</span>, {experience.laravel} years with{' '}
+        <span class="text--laravel">Laravel</span> and {experience.aws} years
+        with <span class="text--aws">AWS</span>.
+      </p>
+
       <p>
         In my free time I work on projects with{' '}
         <span class="text--rust">Rust</span>, <span class="text--go">Go</span>{' '}
@@ -28,7 +57,7 @@ export function Page() {
         <a href="https://github.com/tortitast" target="_blank">
           Github
         </a>{' '}
-        to see the rest of my work.
+        or <a href="/projects">projects page</a> to see the rest of my work.
       </p>
     </main>
   )
