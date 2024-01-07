7c989481f1530fc5efa70a73c1722c73cec0ffef
Victor Garcia <victorgf2011@gmail.com>
2024-01-07 17:52:50 +0100

    feat: pastel colors about

--- pages/about/+Page.jsx
+++ pages/about/+Page.jsx
@@ -15,49 +15,47 @@ export function Page() {
     dateDiffDays(new Date(2022, 2, 1), new Date())
   )
 
-  const experience = {
-    nuxt: descomExperience,
-    laravel: descomExperience,
-    aws: descomExperience,
-  }
-
   return (
     <>
       <main className="p-6 max-w-xl text-justify mix-blend-exclusion">
         <h1>about</h1>
 
         <p>
-          Hi, I'm Victor Garcia, a {age} years old web developer, indie game
+          Hi, I'm Victor Garcia, a {age}-year-old web developer, indie game
           developer and programming enthusiast.
         </p>
 
         <p>
-          I work as a developer at{' '}
+          I currently work at{' '}
           <a href="https://descom.es" target="_blank">
             Descom.es
           </a>
-          , a software company based on La Nucia, Spain, where I work with{' '}
-          <span class="text--aws">AWS</span>,{' '}
-          <span class="text--nuxt">Nuxt.js</span> and{' '}
-          <span class="text--laravel">Laravel</span>.
+          , a software company based on La Nucia, Spain.
+          {/* where I work with{' '} */}
+          {/* <span class="text--aws">AWS</span>,{' '} */}
+          {/* <span class="text--nuxt">Nuxt.js</span> and{' '} */}
+          {/* <span class="text--laravel">Laravel</span>. */}
         </p>
 
         <p>
-          I currently have {experience.nuxt} years of experience with{' '}
-          <span class="text--nuxt">Nuxt.js</span>, {experience.laravel} years
-          with <span class="text--laravel">Laravel</span> and {experience.aws}{' '}
-          years with <span class="text--aws">AWS</span>.
+          I have {descomExperience} years of professional experience with{' '}
+          <span class="text--nuxt">Nuxt.js</span>,{' '}
+          <span class="text--laravel">Laravel</span> and{' '}
+          <span class="text--aws">AWS</span>.
         </p>
 
         <p>
-          In my free time I work on projects with{' '}
+          In my free time, I work on projects with{' '}
           <span class="text--rust">Rust</span>, <span class="text--go">Go</span>{' '}
-          and other interesting technologies. You can check out all my games on
-          my{' '}
+          and other interesting technologies.
+        </p>
+
+        <p>
+          You can check out all my games on my{' '}
           <a href="https://tortitas.itch.io/" target="_blank">
             itch.io
           </a>{' '}
-          profile or head to my{' '}
+          profile, head to my{' '}
           <a href="https://github.com/tortitast" target="_blank">
             Github
           </a>{' '}
