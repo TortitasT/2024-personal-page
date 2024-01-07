f9d2b2f38ffb7db2ee88704f0d71873c57c304c2
Victor Garcia <victorgf2011@gmail.com>
2024-01-07 16:39:37 +0100

    feat: vercel api route

--- renderer/+config.h.js
+++ renderer/+config.h.js
@@ -1,7 +1,4 @@
-import vercelConfig from '@vite-plugin-vercel/vike/config'
-
 export default {
   // See https://vike.dev/data-fetching
   passToClient: ['pageProps', 'urlPathname', 'routeParams'],
-  extends: vercelConfig,
 }
