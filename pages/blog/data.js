f8c9574171f77bf3cf251cd348c0689ab40cbe55
Victor Garcia <victorgf2011@gmail.com>
2024-01-09 16:55:49 +0100

    refactor: use glob to fetch the posts instead of a plain json file

--- pages/blog/data.js
+++ pages/blog/data.js
@@ -1,14 +1,36 @@
 import { render } from 'vike/abort'
-import posts from '../../blog/published.json'
+
+function dateToLangFormat(post) {
+  return {
+    ...post,
+    date: new Intl.DateTimeFormat('en-GB', {
+      dateStyle: 'full',
+      timeZone: 'Europe/Madrid',
+    }).format(new Date(post.date)),
+  }
+}
+
+function sortByDate(a, b) {
+  return new Date(b.date) - new Date(a.date)
+}
 
 export async function data(pageContext) {
+  const postsPromise = import.meta.glob('./../../blog/*.mdx')
+
+  const posts = await Promise.all(
+    Object.entries(postsPromise).map(async ([filename, promise]) => {
+      const { default: Component, frontmatter } = await promise()
+
+      return {
+        slug: filename.replace('./../../blog/', '').replace('.mdx', ''),
+        title: frontmatter.title,
+        description: frontmatter.description,
+        date: frontmatter.date,
+      }
+    })
+  )
+
   return {
-    posts: posts.map((post) => ({
-      ...post,
-      date: new Intl.DateTimeFormat('en-GB', {
-        dateStyle: 'full',
-        timeZone: 'Australia/Sydney',
-      }).format(new Date(post.date)),
-    })),
+    posts: posts.map(dateToLangFormat).sort(sortByDate),
   }
 }
