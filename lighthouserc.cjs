db15071de04fdad9773beccd27d8935e95cc334a
Tortitas <victorgf2011@gmail.com>
2024-01-16 13:38:50 +0100

    fix: lh actions

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -4,8 +4,14 @@ module.exports = {
       url: ['http://localhost:3000/'],
       startServerCommand: 'pnpm run preview',
     },
+    upload: {
+      target: "temporary-public-storage",
+    },
     assert: {
       preset: 'lighthouse:recommended',
+      upload: {
+        target: "temporary-public-storage",
+      },
     },
   },
 };
